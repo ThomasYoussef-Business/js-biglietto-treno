8a4f9753cc8a29fb347bad4c486b1beef401e045
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-10 15:24:40 +0100

    Finito l'esercizio

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 "use strict";
-let chilometriViaggio, etaUtente;
+let chilometriViaggio, etaUtente, prezzoFinale, sconto;
 
 etaUtente = parseInt(prompt("Per favore inserisci la tua età: "));
 while (isNaN(etaUtente) || etaUtente < 3) {
@@ -14,3 +14,19 @@ while (isNaN(chilometriViaggio) || chilometriViaggio <= 0) {
     I bambini sotto ai 3 anni possono viaggiare senza biglietto.
     Per favore inserisci un'età sopra a 3 anni: `));
 }
+
+if (etaUtente >= 65) {
+    sconto = 0.60;
+} else if (etaUtente < 18) {
+    sconto = 0.80;
+} else {
+    sconto = 1
+};
+
+prezzoFinale = chilometriViaggio * 0.21 * sconto;
+prezzoFinale = Math.round(prezzoFinale * 100) / 100;
+
+console.log(`
+    Per viaggiare ${chilometriViaggio}, ed avendo ${etaUtente} anni,
+    Il prezzo finale è di €${prezzoFinale}
+`);
