7c25960901536ae73089a5cfa3fe0b032b37210b
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-10 15:16:54 +0100

    Aggiunto input chilometri

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 "use strict";
 let chilometriViaggio, etaUtente;
 
-etaUtente = parseInt(prompt("Per favore inserisci la tua età: "))
+etaUtente = parseInt(prompt("Per favore inserisci la tua età: "));
 while (isNaN(etaUtente) || etaUtente < 3) {
     etaUtente = parseInt(prompt(`L'età inserita non è un numero valido!
     I bambini sotto ai 3 anni possono viaggiare senza biglietto.
-    Per favore inserisci un'età sopra a 3 anni: `))
+    Per favore inserisci un'età sopra a 3 anni: `));
+}
+
+chilometriViaggio = parseFloat(prompt("Per favore inserisci i chilometri che desidera viaggiare: "));
+while (isNaN(chilometriViaggio) || chilometriViaggio <= 0) {
+    chilometriViaggio = parseFloat(prompt(`L'età inserita non è un numero valido!
+    I bambini sotto ai 3 anni possono viaggiare senza biglietto.
+    Per favore inserisci un'età sopra a 3 anni: `));
 }
