4a269ea25425c02d5fd084c9e3fb5610598c71f0
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-10 15:15:06 +0100

    Aggiunto input età

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 "use strict";
+let chilometriViaggio, etaUtente;
+
+etaUtente = parseInt(prompt("Per favore inserisci la tua età: "))
+while (isNaN(etaUtente) || etaUtente < 3) {
+    etaUtente = parseInt(prompt(`L'età inserita non è un numero valido!
+    I bambini sotto ai 3 anni possono viaggiare senza biglietto.
+    Per favore inserisci un'età sopra a 3 anni: `))
+}
