f06903bb988b6643c1019bef45225a18d6812cf9
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-11 15:24:33 +0100

    Convertite le estensioni ad una funzione

--- js/main.js
+++ js/main.js
@@ -1,31 +1,25 @@
 "use strict";
 /* ----------------- This method makes it so you don't need ----------------- */
 /* ---------- a while loop to get float or int input from the user ---------- */
-String.prototype.ext_promptForParsableFloat = function (errorMessage, keepAskingIfTrue) {
-    let internalStringPlaceholder; // Strings are immutable, so we make our own placeholder
-    const currentString = this.valueOf();
-    const parsedValue = parseFloat(currentString);
+function ensureFloatFromPrompt(firstPromptMessage, errorMessage, keepAskingIfTrue) {
+    let parsedValue = parseFloat(prompt(firstPromptMessage));
     if (!isNaN(parsedValue) && !keepAskingIfTrue(parsedValue)) {
         return parsedValue; // If the parsed value is all good, just return that
     }
 
-    // Otherwise, we start working with our placeholder
-    while (isNaN(parseFloat(internalStringPlaceholder)) ||
-        keepAskingIfTrue(parseFloat(internalStringPlaceholder))) {
-        internalStringPlaceholder = prompt(errorMessage);
+    // Otherwise, keep asking until it is
+    while (isNaN(parseFloat(parsedValue)) ||
+        keepAskingIfTrue(parseFloat(parsedValue))) {
+        parsedValue = prompt(errorMessage);
     }
 
-    return parseFloat(internalStringPlaceholder);
+    return parseFloat(parsedValue);
 }
 
-/* --------------------------- Various extensions -------------------------- */
-String.prototype.ext_promptForParsableInt = function (errorMessage, keepAskingIfTrue) {
-    return parseInt(this.ext_promptForParsableFloat(errorMessage, keepAskingIfTrue));
+// To get an integer version, let's just get the float version and parse to int
+function ensureIntFromPrompt(firstPromptResult, errorMessage, keepAskingIfTrue) {
+    return parseInt(ensureFloatFromPrompt(firstPromptResult, errorMessage, keepAskingIfTrue));
 }
-
-Number.prototype.ext_promptForParsableFloat = String.prototype.ext_promptForParsableFloat
-Number.prototype.ext_promptForParsableInt = String.prototype.ext_promptForParsableInt
-
 
 /* -------------------------------------------------------------------------- */
 /* Main script's logic                                                        */
@@ -35,12 +29,11 @@ let chilometriViaggio, etaUtente, prezzoFinale, sconto;
 const ageInputError = `L'età inserita non è un numero valido
 I bambini sotto ai 3 anni possono viaggiare senza biglietto.
 Per favore inserisci un 'età sopra a 3 anni: `;
-etaUtente = prompt("Per favore inserisci la tua età: ")
-    .ext_promptForParsableInt(ageInputError, (x) => x < 3);
+etaUtente = ensureIntFromPrompt("Per favore inserisci la tua età: ", ageInputError, (x) => x < 3)
 
 const kilometersInputError = `Il numero di chilometri inserito non è valido!
 I chilometri devono essere più di zero: `;
-chilometriViaggio = prompt("Per favore inserisci i chilometri che desidera viaggiare: ").ext_promptForParsableFloat(kilometersInputError, (x) => x <= 0);
+chilometriViaggio = ensureFloatFromPrompt("Per favore inserisci i chilometri che desidera viaggiare: ", kilometersInputError, (x) => x <= 0)
 
 if (etaUtente >= 65) {
     sconto = 0.60;
@@ -54,6 +47,6 @@ prezzoFinale = chilometriViaggio * 0.21 * sconto;
 prezzoFinale = Math.round(prezzoFinale * 100) / 100;
 
 console.log(`
-    Per viaggiare ${chilometriViaggio}, ed avendo ${etaUtente} anni,
+    Per viaggiare ${chilometriViaggio} kilometri, ed avendo ${etaUtente} anni,
     Il prezzo finale è di €${prezzoFinale}
 `);
